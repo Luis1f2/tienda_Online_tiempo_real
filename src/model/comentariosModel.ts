f03bdccffd9c656d70f2798af1174b560dff1946
Luis1f2 <flores123ruiz2316@gmail.com>
2024-06-07 10:52:55 -0600

    acutalizacion de models

--- src/model/comentariosModel.ts
+++ src/model/comentariosModel.ts
@@ -1,14 +1,12 @@
 import { Schema, model, Document, Types } from 'mongoose';
 
 interface IComment extends Document {
-  productId: Types.ObjectId;
   username: string;
   message: string;
   replies: Types.ObjectId[];
 }
 
 const commentSchema = new Schema<IComment>({
-  productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
   username: { type: String, required: true },
   message: { type: String, required: true },
   replies: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
